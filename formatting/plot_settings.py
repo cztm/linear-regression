afda996b14a0823ff41bac332d0fb1984df2cf08
Emma Juan Salazar <emmajuanslz@gmail.com>
2024-10-12 18:00:10 -0700

    writing the paper

---
+++ formatting/plot_settings.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+
+plt.rcParams.update({
+    'figure.figsize': (12, 8),
+    'axes.titlesize': 16,
+    'axes.labelsize': 14,
+    'xtick.labelsize': 10,
+    'ytick.labelsize': 10,
+    'legend.fontsize': 10
+})
+
+colors = {
+    "purple":"#838ceb",
+    "blue":"#6495ed",
+    "red": "#FF0000",
+    "custom_purples" : [
+    "#c2c7f2",
+    "#959cdb",
+    "#838ceb",  
+    "#6973db",  
+    "#4a54b5"],
+    "custom_blues": [
+        "#6baed6",
+        "#4292c6",
+        "#2171b5",
+        "#08519c",
+        "#08306b"
+    ]
+}
